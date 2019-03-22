320c45416fb045a3e36681bc1ab1f97bf5f3cd0b
tazzmaniac <tazzmaniac51@gmail.com>
2019-03-22 22:01:10 +0100

    request.php file added

---
+++ src/server/request.php
@@ -0,0 +1,46 @@
+<?php
+
+//api token bot
+$botToken = " ";
+$website = "https://api.telegram.org/bot".$botToken;
+
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+
+$chatId = $update["message"]["chat"]["id"];
+$chatType = $update["message"]["chat"]["type"];
+
+$message = $update["message"]["text"];
+
+switch ($message) {
+    case '/command':
+        $response = "Este es el mensaje del bot al comando.";
+        sendMessage($chatId, $response);
+        break;
+    case '/noticias':
+        getNews($chatId);
+        break;
+}
+
+function sendMessage($chatId, $message){
+    $url = $GLOBALS[website].'/command?chat_id'.$chatId.'&parse_mode=HTML'.urlencode($response);
+    file_get_contents($url);
+}
+function getNews($chatId){
+    include("simple_html_dom.php");
+    $context = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
+    
+    $url = "http://feeds.weblogssl.com/genbeta";
+    $xmlstring = file_get_contents($url, false, $context);
+    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+    $json = json_encode($xml);
+    
+    $array = json_decode($json, TRUE);
+    for ($i=0; $i < 9 ; $i++) { 
+        $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'>
+        +info</a>";
+    }
+    
+    sendMessage($chatId, $titulos);
+}
+?>
